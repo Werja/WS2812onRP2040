6bb4ed284d1eca1e3c8d0c7bdec85f8c570053cd
Werja <Werner.J@rmatz.de>
2021-04-12 18:39:07 +0200

    Add files via upload

--- example1.py
+++ example1.py
@@ -1,19 +1,21 @@
 import neopixel2040 as np
 import utime
 # the 100 pixel stripe needs 3ms for the ws2812 outputting on pin
-# 360 x 3ms = 1080ms output time
+# 360 x 3ms = pure 1080ms output time
 strip1 = np.strip(100,22,0.1)
-start =  utime.ticks_ms()
-np_thread_active = True
-for i in range(360):
-    strip1.fill(np.hue2col(i))
-print("script runtime",utime.ticks_ms()-start,"ms without outputting")
+
 start =  utime.ticks_ms()
 for i in range(360):
     strip1.fill(np.hue2col(i))
     strip1.show()
 print("script runtime",utime.ticks_ms()-start,"ms with outputting")
-print("core0 puts",np.core0time)
-print("core1 puts",np.core1time)
+
 np.Wait4ThreadEnd()
 strip1.clear()
+
+print("core0 puts",np.core0time)
+print("core1 puts",np.core1time)
+
+# The pixel manipulation (strip1.fill(np.hue2col(i))) runs on core 0
+# while the outputting on core 1 runs
+# If more then one stripe is defind, the output is distributed to both cores. 
